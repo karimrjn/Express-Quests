966dbb865a25cfbc0582d95bdc4426abb71e1633
Karim Rejani <karim.rjn02@gmail.com>
2023-11-27 15:59:40 +0100

    finished challenge POST express quest 2.1

--- tests/movies.test.js
+++ tests/movies.test.js
@@ -1,8 +1,6 @@
 const request = require("supertest");
-
 const app = require("../src/app");
-
-const database = require("../database")
+const database = require("../database");
 
 afterAll(() => database.end());
 
@@ -10,9 +8,8 @@ describe("GET /api/movies", () => {
   it("should return all movies", async () => {
     const response = await request(app).get("/api/movies");
 
-    expect(response.headers["content-type"]).toMatch(/json/);
-
     expect(response.status).toEqual(200);
+    expect(response.headers["content-type"]).toMatch(/json/);
   });
 });
 
@@ -20,14 +17,56 @@ describe("GET /api/movies/:id", () => {
   it("should return one movie", async () => {
     const response = await request(app).get("/api/movies/1");
 
-    expect(response.headers["content-type"]).toMatch(/json/);
-
     expect(response.status).toEqual(200);
+    expect(response.headers["content-type"]).toMatch(/json/);
   });
 
   it("should return no movie", async () => {
     const response = await request(app).get("/api/movies/0");
 
     expect(response.status).toEqual(404);
+  });
+});
+
+describe("POST /api/movies", () => {
+  it("should return created movie", async () => {
+    const newMovie = {
+      title: "Star Wars",
+      director: "George Lucas",
+      year: "1977",
+      color: true,
+      duration: 120,
+    };
+
+    const response = await request(app).post("/api/movies").send(newMovie);
+
+    expect(response.status).toEqual(201);
+    expect(response.body).toHaveProperty("id");
+
+    const [result] = await database.query(
+      "SELECT * FROM movies WHERE id=?",
+      response.body.id
+    );
+
+    const [movieInDatabase] = result;
+
+    expect(movieInDatabase).toMatchObject({
+      id: response.body.id,
+      title: newMovie.title,
+      director: newMovie.director,
+      year: newMovie.year,
+      color: newMovie.color,
+      duration: newMovie.duration,
+    });
+  });
+
+  it("should return an error", async () => {
+    const movieWithMissingProps = { title: "Harry Potter" };
+
+    const response = await request(app)
+      .post("/api/movies")
+      .send(movieWithMissingProps);
+
+    expect(response.status).toEqual(500);
   });
 });
--- tests/users.test.js
+++ tests/users.test.js
@@ -1,18 +1,43 @@
+const crypto = require("node:crypto");
 const request = require("supertest");
-
 const app = require("../src/app");
-
-const database = require("../database")
+const database = require("../database");
 
 afterAll(() => database.end());
+
+describe("POST /api/users", () => {
+  it("should return created user", async () => {
+    const newUser = {
+      firstname: "Marie",
+      lastname: "Martin",
+      email: `${crypto.randomUUID()}@wild.co`,
+      city: "Paris",
+      language: "French",
+    };
+
+    const response = await request(app)
+      .post("/api/users")
+      .send(newUser)
+      .set("Accept", "application/json");
+
+    expect(response.headers["content-type"]).toMatch(/json/);
+    expect(response.status).toEqual(201);
+    expect(response.body).toHaveProperty("id");
+    expect(response.body.firstname).toEqual(newUser.firstname);
+    expect(response.body.lastname).toEqual(newUser.lastname);
+    expect(response.body.email).toEqual(newUser.email);
+    expect(response.body.city).toEqual(newUser.city);
+    expect(response.body.language).toEqual(newUser.language);
+  });
+});
 
 describe("GET /api/users", () => {
   it("should return all users", async () => {
     const response = await request(app).get("/api/users");
 
     expect(response.headers["content-type"]).toMatch(/json/);
-
     expect(response.status).toEqual(200);
+    expect(response.body).toBeInstanceOf(Array);
   });
 });
 
@@ -21,8 +46,8 @@ describe("GET /api/users/:id", () => {
     const response = await request(app).get("/api/users/1");
 
     expect(response.headers["content-type"]).toMatch(/json/);
-
     expect(response.status).toEqual(200);
+    expect(response.body).toHaveProperty("id");
   });
 
   it("should return no user", async () => {
