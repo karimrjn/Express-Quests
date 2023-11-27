f42b402a19dbfeebea36e735b679cb24d67a6240
Karim Rejani <karim.rjn02@gmail.com>
2023-11-27 12:31:45 +0100

    finished challenge quest express 01.2

--- src/app.js
+++ src/app.js
@@ -1,10 +1,16 @@
+require("dotenv").config();
 const express = require("express");
-
 const app = express();
 
+const port = process.env.APP_PORT;
+
 const movieControllers = require("./controllers/movieControllers");
+const userControllers = require("./controllers/userControllers");
 
 app.get("/api/movies", movieControllers.getMovies);
 app.get("/api/movies/:id", movieControllers.getMovieById);
+app.get("/api/users", userControllers.getUsers);
+app.get("/api/users/:id", userControllers.getUserById);
+
 
 module.exports = app;
---
+++ src/controllers/userControllers.js
@@ -0,0 +1,36 @@
+const database = require("../../database");
+
+const getUsers = (req, res) => {
+    database
+    .query("SELECT * FROM users")
+    .then(([users]) => {
+      res.status(200).json(users);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+const getUserById = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  database
+    .query("SELECT * FROM users WHERE id = ?", [id])
+    .then(([users]) => {
+      if (users[0] !== undefined) {
+        res.json(users[0]);
+      } else {
+        res.sendStatus(404);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+module.exports = {
+  getUsers,
+  getUserById,
+};
